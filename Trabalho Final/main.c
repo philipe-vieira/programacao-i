148cfa33322fec2eb5943f475a7e472720c97941
Philipe Vieira <philipefilho@hotmail.com>
2021-04-15 01:47:49 -0300

    Esqueleto do projeto

---
+++ Trabalho Final/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Struc para o cliente
+typedef{
+	int cpf;
+	char nome[30];
+}Cliente
+// Struct para a passagem
+typedef {
+	int assento;
+	Cliente passageiro;
+}Passagem;
+
+// Função para imprimir menu
+
+// Função para cadastrar o cliente
+
+// Função para alterar o cadastro do cliente
+
+// Função para adquirir passagem
+
+// Função para alterar passagem
+
+// Função para devolver passagem
+
+// Função para listar os passageiros
+
+// Função para listar os assentos 
+
+// Função para listar a qnt de assentos e o valor a pagar
+
+int main (){
+	
+	return 0;
+}
